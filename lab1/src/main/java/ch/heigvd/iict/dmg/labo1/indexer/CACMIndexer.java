cc8cf357a3b76d5628ed9601bfb97df5676f0d3d
Romain Capocasale <romain.capocasale99@gmail.com>
2020-10-03 18:10:21 +0200

    add

--- lab1/src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
+++ lab1/src/main/java/ch/heigvd/iict/dmg/labo1/indexer/CACMIndexer.java
@@ -20,64 +20,79 @@ import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 public class CACMIndexer implements ParserListener {
-	
-	private Directory 	dir 			= null;
-	private IndexWriter indexWriter 	= null;
-	
-	private Analyzer 	analyzer 		= null;
-	private Similarity 	similarity 		= null;
-	
+
+	private Directory dir = null;
+	private IndexWriter indexWriter = null;
+
+	private Analyzer analyzer = null;
+	private Similarity similarity = null;
+
 	public CACMIndexer(Analyzer analyzer, Similarity similarity) {
 		this.analyzer = analyzer;
 		this.similarity = similarity;
 	}
-	
+
 	public void openIndex() {
 		// 1.2. create an index writer config
 		IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
 		iwc.setOpenMode(OpenMode.CREATE); // create and replace existing index
-		iwc.setUseCompoundFile(false); // not pack newly written segments in a compound file: 
-		//keep all segments of index separately on disk
-		if(similarity != null)
+		iwc.setUseCompoundFile(false); // not pack newly written segments in a compound file:
+		
+		// keep all segments of index separately on disk
+		if (similarity != null)
 			iwc.setSimilarity(similarity);
 		// 1.3. create index writer
 		Path path = FileSystems.getDefault().getPath("index");
 		try {
 			this.dir = FSDirectory.open(path);
 			this.indexWriter = new IndexWriter(dir, iwc);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void onNewDocument(Long id, String authors, String title, String summary) {
-		Document doc = new Document();
+		if (summary != null) {
 
-		// TODO student: add to the document "doc" the fields given in
-		// parameters. You job is to use the right Field and FieldType
-		// for these parameters.
-		
-		doc.add(new TextField("authors", authors, Field.Store.YES));
-		doc.add(new TextField("title", title, Field.Store.YES));
-		
-		FieldType fieldType = new FieldType(); 
-		fieldType.setIndexOptions(IndexOptions.DOCS);
-		doc.add(new Field("summary", summary, fieldType));
+			Document doc = new Document();
+
+			// TODO student: add to the document "doc" the fields given in
+			// parameters. You job is to use the right Field and FieldType
+			// for these parameters.
+
+			doc.add(new TextField("authors", authors, Field.Store.YES));
+			doc.add(new TextField("title", title, Field.Store.YES));
+
+			FieldType fieldType = new FieldType();
+			fieldType.setIndexOptions(IndexOptions.DOCS);
+			fieldType.setStoreTermVectorOffsets(true);
+			fieldType.setStoreTermVectors(true);
+			doc.add(new Field("summary", summary, fieldType));
 
 
-		try {
-			this.indexWriter.addDocument(doc);
-		} catch (IOException e) {
-			e.printStackTrace();
+			try {
+				this.indexWriter.addDocument(doc);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
+
 	}
-	
+
 	public void finalizeIndex() {
-		if(this.indexWriter != null)
-			try { this.indexWriter.close(); } catch(IOException e) { /* BEST EFFORT */ }
-		if(this.dir != null)
-			try { this.dir.close(); } catch(IOException e) { /* BEST EFFORT */ }
+		if (this.indexWriter != null)
+			try {
+				this.indexWriter.close();
+			} catch (IOException e) {
+				/* BEST EFFORT */ }
+		if (this.dir != null)
+			try {
+				this.dir.close();
+
+			} catch (IOException e) {
+				/* BEST EFFORT */ }
 	}
-	
+
 }
